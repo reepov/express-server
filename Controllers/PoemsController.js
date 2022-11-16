6816522a7e229cbceeedcbed0784e45e9ed17769
mikhail <mix.reep@mail.ru>
2022-11-16 18:42:49 +0300

    some changes

--- Controllers/PoemsController.js
+++ Controllers/PoemsController.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const PoemsRouter = express.Router();
+var multer = require('multer');
+var upload = multer();
 const Users = require('../Models/UserModel')
 const Poems = require('../Models/PoemsModel');
 const { Sequelize } = require('sequelize');
 const UserViewModel = require("../ViewModels/UserViewModel");
 const PoemsViewModel = require('../ViewModels/PoemsViewModel');
+const router = require("./UserController");
 const db = new Sequelize('postgresql://postgres:postgres@185.119.56.91:5432/postgres');
 Users.sync();
 Poems.sync();
@@ -28,5 +32,14 @@ PoemsRouter.get("/GetListOfRandomPoems", function(_req, res){
         res.send(poemsToSend.sort((a, b) => Number(a.isLikedByCurrentUser) - Number(b.isLikedByCurrentUser)));
     });
 });
+
+PoemsRouter.post("/AuthorSendPoem", function(req, res){
+    db.sync();
+    const Title = req.query.title
+    const userId = req.query.userId
+    const text = req.body.text.trimEnd(']').trimStart('[').split("|, ");
+    res.send(text);
+
+})
 
 module.exports = PoemsRouter;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const UserViewModel = require('../express-server/ViewModels/UserViewModel');
 const PoemsViewModel = require('../express-server/ViewModels/PoemsViewModel');
 const express = require('express');
+const bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 const { Sequelize } = require('sequelize');
 const db = new Sequelize('postgresql://postgres:postgres@185.119.56.91:5432/postgres');
 const User = require('../express-server/Models/UserModel');
@@ -8,6 +11,17 @@ const Poems = require('../express-server/Models/PoemsModel');
 const host = 'localhost';
 const port = 3333;
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array()); 
+app.use(express.static('public'));
 const UserController = require('../express-server/Controllers/UserController');
 const PoemsController = require('../express-server/Controllers/PoemsController');
 const UUIDV4 =  require('uuid');
