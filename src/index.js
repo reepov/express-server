139aa1bba61a8ecc90e48cd73d611a3531f742a9
Mikhail Strakhovskiy <mix.reep@mail.ru>
2022-11-18 02:18:20 +0300

    before dockering

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const UUIDV4 =  require('uuid');
+const multer = require('multer');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Sequelize } = require('sequelize');
+const User = require('./Models/UserModel');
+const Poems = require('./Models/PoemsModel');
+const UserViewModel = require('./ViewModels/UserViewModel');
+const PoemsViewModel = require('./ViewModels/PoemsViewModel');
+const UserController = require('./Controllers/UserController');
+const PoemsController = require('./Controllers/PoemsController');
+const db = new Sequelize('postgresql://postgres:postgres@185.119.56.91:5432/postgres');
+
+const port = 3333;
+const app = express();
+const upload = multer();
+app.use(upload.array()); 
+const host = 'localhost';
+app.use(bodyParser.json()); 
+app.use(express.static('public'));
+app.use('/api/User/', UserController);
+app.use('/api/Poems/', PoemsController);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(port, () => console.log('Application listening on port 3333!'));
